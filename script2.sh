3bbe9fd86dc98cbfe9caf8fe28ad2ba3b27d19d8
beamoriim <beamorim2606@gmail.com>
2022-06-15 03:20:24 +0000

    lista4

--- script2.sh
+++ script2.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
-declare -A d
-while read usr ur1 qwdn
-do
-	d["$usr"]=$(( ${d["$usr"]} += $qdwn ))
-done < $1
-for usr in ${!d[*]}
-do
-	echo "$usr ${d[${usr}]}"
+for usuario in $(cat $1 | cut -d " " -f 1 | sort | uniq); do
+	soma=0
+	for dados in $(cat $1 | grep "$usuario" | cut -d " " -f 3); do
+		soma=$((soma+dados))
+	done
+        	echo "$usuario, $soma"
 done
--- script4.sh
+++ script4.sh
@@ -1,3 +1,9 @@
 #!/bin/bash 
-  
 
+# (padrão) -> dentro dos parenteses.
+# [0-9] -> qualquer dígito de 0 até 9.
+# [0-9]{1,3} -> qualquer dígito de 0 até 9, ocorrendo 0 -> 19 -> 192
+# \. -> escapando (transforma em string) caractere ponto.
+# ([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}) vai pegar qualquer ip.
+ 
+cat $1 | sed -E 's/([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3})/**!!CEN-SU-RA-DO!!**/'
